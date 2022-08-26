2f7983f8b30875e46344e39337ac35e1ae9512ab
USER 2020 <hackingetico2021@gmail.com>
2022-08-26 01:13:26 -0500

    Creacion de carpeta build modo producion minuto 01:35:00

--- database.js
+++ database.js
@@ -46,7 +46,27 @@ app.post('/registrar', function(req, res){
 
 });
 
-app.listen(5000, function(){
+//Usar estaicos en modo produccion 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('frontend/build'));
+    app.get("*", (req, res) => {
+        res.sendFile((__dirname + "/frontend/build/index.html"));
+    })
+
+}
+
+//CAmbio de puerto para proxy heroku 
+let port = process.env.PORT;
+if(port == null || port == ""){
+    port = 5000;
+}
+
+
+
+//port for database
+//Puede hacerse Cambiando el puerto de nuevo y añadiendo el proxy en registro 
+//y cambiando el valor de port por 5000
+app.listen(port, function(){
     console.log("server express on port 5000");
 })
 
